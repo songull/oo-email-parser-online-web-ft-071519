0fe621d7d6f5d38673c90528663ed9159690cc79
Kyler Barber <kylerbarber820@yahoo.com>
2019-08-13 22:13:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,24 +1,14 @@
 class EmailParser
-  attr_accessor :name, :csv_emails
 
-  def initialize(csv_emails)
-
-    @csv_emails = csv_emails
+  attr_accessor :email
+  def  initialize(emails)
+    @email = emails
   end
-
 
   def parse
-    csv_emails.split.collect do |address|
-      address.split(',')
-    end
-    .flatten.uniq
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
   end
+
 end
-
-emails1 = "asd@qw.com, per@er.org, Zaza@sugar.net"
-parser1 = EmailParser.new(emails1)
-parser1.parse
-
-emails2 = "asd@qw.com per@er.org per@er.org Zaza@sugar.net"
-parser2 = EmailParser.new(emails2)
-parser2.parse
